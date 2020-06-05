3a8d43cc5e554164e11e35ae70743c7d84c02cd4
FeedThePiano <jokup@t-online.de>
2020-06-05 17:33:26 +0200

    Updates

---
+++ precache-manifest.1c844da27b41a96c8c6639488dcf9a74.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc13d23ecd34f4832168f38707dd3c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de55b2a691d18868abf4",
+    "url": "/static/css/main.2a00ae09.chunk.css"
+  },
+  {
+    "revision": "c506b8023ce3f7f4cee9",
+    "url": "/static/js/2.b53a3f96.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.b53a3f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de55b2a691d18868abf4",
+    "url": "/static/js/main.0d3538cb.chunk.js"
+  },
+  {
+    "revision": "a0476513119a920096fc",
+    "url": "/static/js/runtime-main.455a73ce.js"
+  },
+  {
+    "revision": "435741a3d0668665b434a68bc6f7de9e",
+    "url": "/static/media/fog.435741a3.png"
+  },
+  {
+    "revision": "f6f8abb8ea9221513e4e00ba1bd627c7",
+    "url": "/static/media/jupiter.f6f8abb8.svg"
+  },
+  {
+    "revision": "6a2e148ddc98290d3a774475c6a0d4a9",
+    "url": "/static/media/moon.6a2e148d.svg"
+  },
+  {
+    "revision": "b5ed6d2961fb89bf6f55042e6f463d07",
+    "url": "/static/media/saturn.b5ed6d29.svg"
+  },
+  {
+    "revision": "3597479ad55eaccb9d5703ae2a2218b2",
+    "url": "/static/media/stars.3597479a.svg"
+  },
+  {
+    "revision": "967d818e2c3c1a71cee1c98b5f2cdfbe",
+    "url": "/static/media/stars2.967d818e.svg"
+  },
+  {
+    "revision": "574eb9580bf177a2d7c108f73ffe68ad",
+    "url": "/static/media/timr65w.574eb958.ttf"
+  },
+  {
+    "revision": "afd0db7641eb9e69e8c8c6916cce8762",
+    "url": "/static/media/uranus.afd0db76.svg"
+  }
+]);
